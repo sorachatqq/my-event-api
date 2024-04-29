9e039f143a8ea1525eb08d7ab33bbf0124b5a93d
sorachatqq <pleumrinrin@gmail.com>
2024-04-29 13:48:06 +0700

    [update]: authentication

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+from random import randint
 from typing import Optional, Union, List, Any
 from wsgiref.validate import validator
-from fastapi import FastAPI, HTTPException, Depends, status, File, UploadFile, Form, Query, Body
+from fastapi import FastAPI, HTTPException, Depends, status, File, UploadFile, Form, Query, Body, Security
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import FileResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -10,10 +11,15 @@ from pydantic import BaseModel, Field, validator
 import os
 import uuid
 from bson import ObjectId
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "a_very_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
-
-
 
 MONGODB_URL = os.getenv("MONGODB_URL")
 UPLOAD_DIRECTORY = "./uploads"
@@ -25,6 +31,7 @@ db = client['my_event_db']
 
 users_collection = db.users
 events_collection = db.events
+registrations_collection = db.registrations
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -46,6 +53,11 @@ users_db = {
 indexes = events_collection.index_information()
 print(indexes)
 
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
 class AgeRange(BaseModel):
     min: Optional[int] = Field(None, ge=0, le=150)
     max: Optional[int] = Field(None, ge=0, le=150)
@@ -58,6 +70,13 @@ class EventCreate(BaseModel):
     age_range_min: Optional[int] = None
     age_range_max: Optional[int] = None
     type: str
+    approved: bool = False
+    is_open_for_registration: bool = True
+
+class EventRegistration(BaseModel):
+    user_id: str
+    event_id: str
+    registration_code: Optional[str] = None
 
 class Location(BaseModel):
     type: str
@@ -99,16 +118,24 @@ class UserSignUp(BaseModel):
 
 
 class User(BaseModel):
-    id: Optional[str] = Field(None, alias="_id")  # The alias is for MongoDB's '_id' field
+    id: Optional[str] = Field(None, alias="_id")
     username: str
     full_name: Optional[str] = None
     email: str
-    disabled: bool = False
+    disabled: bool = False  # Set a default value
+    role: str = "user"
 
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
+        json_encoders = {
+            ObjectId: lambda v: str(v),
+        }
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    user: User
 
 class UserInDB(BaseModel):
     username: str
@@ -134,6 +161,16 @@ class NearbySearch(BaseModel):
     longitude: float = Field(..., ge=-180.0, le=180.0, description="Longitude of the location")
     radius: float = Field(..., description="Radius in kilometers to search for nearby events")
 
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 @app.post("/signup", response_model=User, tags=["authentication"])
 def create_user(user: UserSignUp):
     if user.password != user.repeat_password:
@@ -142,35 +179,67 @@ def create_user(user: UserSignUp):
             detail="Passwords do not match",
         )
     if users_collection.find_one({"username": user.username}):
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username already registered")
+    if users_collection.find_one({"email": user.email}):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already registered")
+
+    # Here you could add additional password strength validation
 
     hashed_password = pwd_context.hash(user.password)
-    user_data = user.dict(exclude={"password", "repeat_password"})  # Exclude plaintext passwords
+    user_data = user.dict(exclude={"password", "repeat_password"})
     user_data["hashed_password"] = hashed_password
 
     result = users_collection.insert_one(user_data)
     user_id = str(result.inserted_id)
     user_data['id'] = user_id
-    return {**user_data, "hashed_password": ""}  # Exclude hashed_password in the response
+    user_data['disabled'] = False
+
+    return {**user_data, "hashed_password": ""}
 
 def get_user(username: str):
-    user_dict = users_collection.find_one({"username": username})
-    if user_dict:
-        user_dict.pop('_id')  # Remove the ObjectId
-        user_dict.pop('hashed_password')  # Remove the hashed password as it's not needed in the User model
-        return UserInDB(**user_dict)
+    user_record = users_collection.find_one({"username": username})
+    if user_record:
+        user_record['_id'] = str(user_record['_id'])
+        return User(**user_record)
+    else:
+        return None  # or raise an HTTPException indicating the user is not found
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def authenticate_user(username: str, password: str):
     user_dict = users_collection.find_one({"username": username})
-    if user_dict and verify_password(password, user_dict["hashed_password"]):
-        user_dict['_id'] = str(user_dict['_id'])  # Convert ObjectId to string
-        return User(**user_dict)
+    if user_dict:
+        if 'disabled' not in user_dict:
+            user_dict['disabled'] = False  # Default to False if not specified
+
+        if verify_password(password, user_dict["hashed_password"]):
+            user_dict['_id'] = str(user_dict['_id'])  # Convert ObjectId to string for consistency
+            return User(**user_dict)
     return None
 
-@app.post("/login", tags=["authentication"])
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        user = get_user(username=username)  # You need to implement the get_user function.
+        if user is None:
+            raise credentials_exception
+        if user.disabled:  # Here you access the 'disabled' attribute of the user object.
+            raise HTTPException(status_code=400, detail="Inactive user")
+    except JWTError:
+        raise credentials_exception
+    return user
+
+@app.post("/token", response_model=Token, tags=["authentication"])
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
@@ -179,19 +248,13 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    token = user.username
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
     user_data = user.dict(exclude={"hashed_password", "_id"})
-    return {"access_token": token, "token_type": "bearer", "user": user_data}
 
-@app.get("/users/me", response_model=User, tags=["authentication"])
-async def read_users_me(current_username: str = Depends(oauth2_scheme)):
-    user_dict = get_user(current_username)
-    if not user_dict:
-        raise HTTPException(status_code=404, detail="User not found")
-    if user_dict.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    user_dict['_id'] = str(user_dict['_id'])  # Convert ObjectId to string
-    return User(**user_dict)
+    return {"access_token": access_token, "token_type": "bearer", "user": user_data}
 
 @app.post("/events/create", tags=["events"])
 async def create_event(
@@ -295,6 +358,26 @@ async def get_all_events():
     events_cursor = events_collection.find({})
     events_list = list(events_cursor)
     return [GetEvent(**event) for event in events_list]
+
+@app.post("/events/register/{event_id}", response_model=EventRegistration, tags=["events"])
+async def register_for_event(event_id: str, current_user: User = Depends(get_current_user)):
+    event = events_collection.find_one({"_id": event_id})
+    if not event or not event.get('is_open_for_registration', False):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="This event is not open for registration.")
+
+    existing_registration = registrations_collection.find_one({"user_id": current_user.id, "event_id": event_id})
+    if existing_registration:
+        return existing_registration  # User already registered, return existing code
+
+    # Register the user and generate a code
+    registration_code = ''.join([str(randint(0, 9)) for _ in range(6)])
+    registration = {
+        "user_id": current_user.id,
+        "event_id": event_id,
+        "registration_code": registration_code
+    }
+    registrations_collection.insert_one(registration)
+    return registration
 
 @app.post("/home/nearby", response_model=List[GetEvent], tags=["home"])
 async def get_nearby_events(search_params: NearbySearch = Body(...)):
