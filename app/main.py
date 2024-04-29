4bb456275793973f12c45d5aa38249149f0afa08
sorachatqq <pleumrinrin@gmail.com>
2024-04-29 19:08:01 +0700

    [fix]: enable cors

--- app/main.py
+++ app/main.py
@@ -21,14 +21,6 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.add_middleware(
     CORSMiddleware,
